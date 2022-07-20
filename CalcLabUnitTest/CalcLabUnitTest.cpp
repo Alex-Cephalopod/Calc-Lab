80c6ceecec63125aa6c5b94d2caa055a45c3fb69
Alex-Cephalopod <76233802+Alex-Cephalopod@users.noreply.github.com>
2022-07-20 16:57:14 -0400

    SetUp somewhat complete

---
+++ CalcLabUnitTest/CalcLabUnitTest.cpp
@@ -0,0 +1,18 @@
+//#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Calc Lab/CalculatorProcessor.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalcLabUnitTest
+{
+	TEST_CLASS(CalcLabUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
---
+++ CalcLabUnitTest/CalcProcessor.cpp
@@ -0,0 +1,17 @@
+#include "CppUnitTest.h"
+#include "../Calc Lab/CalculatorProcessor.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalcProcessor
+{
+	TEST_CLASS(CalcLabUnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
